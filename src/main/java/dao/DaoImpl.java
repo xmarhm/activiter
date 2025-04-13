1bd5237b587b36f97b106937fd441ab7f5821052
xmarhm <hamou.omar820@gmail.com>
2025-04-13 18:13:24 +0100

    3 commit

--- src/main/java/dao/DaoImpl.java
+++ src/main/java/dao/DaoImpl.java
@@ -1,5 +1,9 @@
 package dao;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+@Repository("dao")
 public class DaoImpl implements IDao {
     @Override
     public double getData() {
--- src/main/java/metier/MetierImpl.java
+++ src/main/java/metier/MetierImpl.java
@@ -1,8 +1,17 @@
 package metier;
+import dao.DaoImpl;
 import dao.IDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
+@Service("metier")
 public class MetierImpl implements IMetier {
+    // @Autowired
+    // @Autowired
+
     private IDao dao;
+
     public MetierImpl(IDao dao) {
         this.dao = dao;
     }
@@ -11,4 +20,8 @@ public class MetierImpl implements IMetier {
         double t = dao.getData();
         return t * 23;
     }
+    /*
+    public void setDao(DaoImpl dao) {
+        this.dao = dao;
+    } */
 }
